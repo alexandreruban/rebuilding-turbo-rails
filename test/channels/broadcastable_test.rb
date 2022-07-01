c6e9168f3dcd329eab1091c6e981ef92b119b9ba
Alexandre Ruban <alexandre@hey.com>
2022-07-01 18:16:53 +0200

    Add tests for the asynchronous methods of the `Broadcastable` concern

--- test/channels/broadcastable_test.rb
+++ test/channels/broadcastable_test.rb
@@ -2,12 +2,23 @@ require "test_helper"
 
 class TurboClone::BroadcastableTest < ActionCable::Channel::TestCase
   include TurboClone::ActionHelper
+  include ActiveJob::TestHelper
 
   test "#broadcast_append_to" do
     article = Article.create! content: "Something"
 
     assert_broadcast_on "stream", turbo_stream_action_tag(:append, target: "articles", template: render(article)) do
       article.broadcast_append_to "stream"
+    end
+  end
+
+  test "#broadcast_append_later_to" do
+    article = Article.create! content: "Something"
+
+    assert_broadcast_on "stream", turbo_stream_action_tag(:append, target: "articles", template: render(article)) do
+      perform_enqueued_jobs do
+        article.broadcast_append_later_to "stream"
+      end
     end
   end
 
@@ -19,11 +30,31 @@ class TurboClone::BroadcastableTest < ActionCable::Channel::TestCase
     end
   end
 
+  test "#broadcast_prepend_later_to" do
+    article = Article.create! content: "Something"
+
+    assert_broadcast_on "stream", turbo_stream_action_tag(:prepend, target: "board_articles", template: render(article)) do
+      perform_enqueued_jobs do
+        article.broadcast_prepend_later_to "stream", target: "board_articles"
+      end
+    end
+  end
+
   test "#broadcast_replace_to" do
     article = Article.create! content: "Something"
 
     assert_broadcast_on "stream", turbo_stream_action_tag(:replace, target: article, template: render(article)) do
       article.broadcast_replace_to "stream"
+    end
+  end
+
+  test "#broadcast_replace_later_to" do
+    article = Article.create! content: "Something"
+
+    assert_broadcast_on "stream", turbo_stream_action_tag(:replace, target: article, template: render(article)) do
+      perform_enqueued_jobs do
+        article.broadcast_replace_later_to "stream"
+      end
     end
   end
 
